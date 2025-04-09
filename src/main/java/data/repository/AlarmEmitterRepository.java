84d8d27e2ad26fcbe471e72d2da78ede611382aa
[sujinpsujinp] <sujinp038@naver.com>
2025-04-09 18:21:45 +0900

    일정 등록 시 열려있는 모든 창에서 알림 발생 및 발생한 알림 db에 저장

--- src/main/java/data/repository/AlarmEmitterRepository.java
+++ src/main/java/data/repository/AlarmEmitterRepository.java
@@ -1,5 +1,7 @@
 package data.repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -10,20 +12,23 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class AlarmEmitterRepository {
 //SseEmitter를 저장하기위한 파일	
 	
-	private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+	private final Map<Long, List<SseEmitter>> emitters = new ConcurrentHashMap<>();
 
 	//userId를 key로 해서 SseEmitter 저장
     public void save(Long userId, SseEmitter emitter) {
-        emitters.put(userId, emitter);
+    	emitters.computeIfAbsent(userId, key -> new ArrayList<>()).add(emitter);
     }
     
     //userId로 사용자의 SseEmitter를 가져오기
-    public SseEmitter get(Long userId) {
-        return emitters.get(userId);
+    public List<SseEmitter> get(Long userId) {
+    	return emitters.getOrDefault(userId, new ArrayList<>());
     }
     
     //userId의 Sse 제거
-    public void remove(Long userId) {
-        emitters.remove(userId);
+    public void remove(Long userId, SseEmitter emitter) {
+    	List<SseEmitter> userEmitters = emitters.get(userId);
+        if (userEmitters != null) {
+            userEmitters.remove(emitter);
+        }
     }
 }
