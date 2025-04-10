39a4a5036e845b0fb6f0e857657df950a9cfe217
jongwon-jeon <jw6963@naver.com>
2025-04-10 20:58:34 +0900

    전자결재 기안문 생성 및 수신함 확인 && header에서 로그인 check && 로그인 시 /으로 이동하도록 수정

--- src/main/java/data/mapper/DraftFilesMapper.java
+++ src/main/java/data/mapper/DraftFilesMapper.java
@@ -1,7 +1,9 @@
 package data.mapper;
 
+import data.dto.DraftFilesDto;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface DraftFilesMapper {
+    public void createFiles(DraftFilesDto draftFilesDto);
 }
